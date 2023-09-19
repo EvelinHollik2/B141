5a8daa5540a03cffec4e9861a9c076fa1bb56426
EvelinHollik2 <hollikevi@gmail.com>
2023-09-19 11:09:10 +0200

    vég

---
+++ OOP/Macska.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Macska : Emlosok
+    {
+        public Macska(string nev, double suly, string nem) : base(nev, "Macska", 4, suly, nem, "Miaúúú")
+        {
+
+        }
+
+
+    }
+}
