d747c47cf97b970b1f659b231fc81b06ff250233
EvelinHollik2 <hollikevi@gmail.com>
2023-11-23 08:53:28 +0100

    grf

---
+++ 2023-11-22_node.js/index.js
@@ -0,0 +1,84 @@
+/*Terminál
+npm -y létrehozza a json csomagot
+npm install express telepíti a többi csomagot
+npm install mysql telepíti az adatbázis kapcsolathoz szügséges csomagokat
+npm install cors telepíti a liveszerver használatához szügséges csomagot
+A mappába létrehoztunk egy fájlt aminek a neve ".gitignore" ennek a tartalma /node_modules/
+node index.js  parancs indítja el a szervert
+Ctrl + C állítja le a szervert
+*/
+//Visual Studio Code-ba a Thunder Client extension-t hozzáadtuk (Ennek a használatában én se tudok segíteni) 
+//Index.js
+const express = require('express'); //express létrehozása
+const app = express(); //-- http szervert tudunk vele indítani
+
+const bodyParser = require('body-parser'); //bodyParser létrehozása
+
+const cors = require('cors');  //cors létrehozása
+app.use(cors());                  //cors használata
+
+app.use(bodyParser.urlencoded({ extended: false })); // bodyParser használata
+app.use(bodyParser.json()); // a bodyban mindig próbálja az adatokat json-é alakítani
+
+const mysql = require('mysql'); //mysql létrehozása
+const { throws } = require('assert');
+const database = mysql.createConnection({ //Database létrehozása
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'tagdij' //ezt az adatbázist használtuk órán
+}
+);
+database.connect((err) => { //Csatlakozás megpróbálása
+    if (err) throw err; //nem sikerült
+    console.log('Connected');//sikerült
+});
+
+app.get('/', (req, res) => { // http://localhost:3000 esetén
+    res.send("Csá öcsi"); // ezt írja ki
+});
+
+app.get('/sanyi', (req, res) => { //  http://localhost:3000/sanyi esetén 
+    res.send("Sanyi öcsém");        // ezt írja ki
+});
+
+app.get('/sanyi/:id', (req, res) => {       //http://localhost:3000/sanyi/"akármilyen id [pl 11]"
+    let id = req.params.id;                 // id request
+    let nev = req.params.nev;               // név request
+    res.send("Sanyi öcsém, id:" + id + `Név: ${nev}`);      //ezt írja ki
+});
+
+app.get('/sanyi/lakcim', (req, res) => {    //http://localhost:3000/sanyi/lakcim
+    res.send("Sanyi öcsém az Északisarkon lakik");   //ezt írja ki
+});
+
+//POST
+
+app.post('/sanyi', (req, res) => { //küldeni sanyinak adatokat
+    let id = req.body.id; //adat
+    let nev = req.body.nev; //adat
+    res.send(`Sanyi öcsém POST, id: ${id}, név: ${nev} `);  //kiírás      
+});
+
+//Adatbázis lekérdezése
+app.get('/tagok', (req, res) => {                   //http://localhost:3000/tagok
+    let sqlcommand = 'SELECT * FROM ugyfel';   //Lekérdezés parancs tárolás
+    database.query(sqlcommand, (err, rows) => {    //Meghívás lekérdezés
+        if (err) throw err;  //nem sikerült
+        res.send(rows); //Sikerült
+    });
+});
+
+app.get('/tagok/:id', (req, res) => {                                      //http://localhost:3000/tagok/[azon]
+    let sqlcommand = `SELECT * FROM ugyfel WHERE azon=${req.params.id}`;   //Lekérdezés azonosító szerint parancs tárolás
+    database.query(sqlcommand, (err, rows) => {                           //Meghívás lekérdezés
+        if (err) throw err;  //nem sikerült
+        res.send(rows); //Sikerült
+    });
+});
+
+
+app.listen(3000, () => {
+    console.log('A szerver fut a 3000-s porton'); //localhost:3000
+});
+// app.listen után nem szabad írni bajos lehet a futása
--- 2023.11.20 AJAX/fetch.js
+++ 2023.11.20 AJAX/fetch.js
@@ -1,11 +1,13 @@
 const inputid = document.querySelector("#id");
-const inputUsernames = document.querySelector("#usernames");
+const inputUsername = document.querySelector("#username");
 const inputDarab = document.querySelector("#darab");
 const buttonRead = document.querySelector("#read");
-const body = document.getElementsByTagName("body")[0];
+const buttonCreate = document.querySelector("#create");
+const buttonUpdate = document.querySelector("#update");
+const buttonDelete = document.querySelector("#delete");
 const cards = document.querySelector("#cards");
 
-body.addEventListener("load", getAllUsers); //-- a lap betöltésekor is..
+window.addEventListener("load", getAllUsers); //-- a lap betöltésekor is..
 buttonRead.addEventListener("click", getAllUsers);
 
 async function getAllUsers() {
@@ -13,22 +15,109 @@ async function getAllUsers() {
     try {
         const response = await fetch(endpoint); //--head, body
         const users = await response.json(); //--body to JSON
-        console.log(users);
+        showAllUsers(users);
     } catch (error) {
         console.log(error);
     }
 }
 
 function showAllUsers(params) {
+    let html = "";
     params.forEach(user => {
-        appendCard(user);
+        html += `<div class="card" style="width: 18rem;">
+        <img src="noimage.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${user.id}. ${user.username}</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                the
+                card's content.</p>
+                <p class="card-text">Darab: ${user.darab}</p>
+            <button class="btn btn-primary" onclick="betoltInputMezobe(${user.id})">Kiválaszt</button>
+        </div>
+    </div>`;
     });
+    cards.innerHTML = html;
 }
 
-function appendCard(user) {
-
+async function betoltInputMezobe(id) {
+    let url = `https://retoolapi.dev/Hfa9uy/data/${id}`;
+    try {
+        const response = await fetch(url);
+        const users = await response.json();
+        inputid.value = users.id;
+        inputUsername.value = users.username;
+        inputDarab.value = users.darab;
+        location.href = "#formEleje";
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-//card="<div class=\"card\" style=\"width: 18rem;\">"
-//+"<img src=\"noimage.jpg\" class=\"card-img-top\" alt=\"...\">"
-//+"<div class=\"card-body\"><h5 class=\"card-title\">Card title</h5>" + user.id + "</h5>";
+//-- új user létrehozása
+buttonCreate.addEventListener("click", async () => {
+    let url = "https://retoolapi.dev/Hfa9uy/data"
+    let data = {
+        username: inputUsername.value,
+        darab: inputDarab.value
+    };
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(data),
+        });
+        const user = await response.json();
+        bevitelimezoAlaphelyzetbe();
+        getAllUsers();
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+//--user felülírása
+buttonUpdate.addEventListener("click", async () => {
+    let url = `https://retoolapi.dev/Hfa9uy/data/${inputid.value}`;
+    let data = {
+        username: inputUsername.value,
+        darab: inputDarab.value
+    };
+    try {
+        const response = await fetch(url, {
+            method: "PUT",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(data),
+        });
+        const user = await response.json();
+        console.log(user);
+        bevitelimezoAlaphelyzetbe();
+        getAllUsers();
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+//-- user törlése
+buttonDelete.addEventListener("click", async () => {
+    let url = `https://retoolapi.dev/Hfa9uy/data/${inputid.value}`;
+    try {
+        const response = await fetch(url, {
+            method: "DELETE",
+        });
+        const user = await response.json();
+        console.log(user);
+        bevitelimezoAlaphelyzetbe();
+        getAllUsers();
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+function bevitelimezoAlaphelyzetbe() {
+    inputid.value ="";
+    inputUsername.value ="";
+    inputDarab.value ="";
+}
