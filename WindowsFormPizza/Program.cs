5957b804a170595eaf1c7010691687b781621630
EvelinHollik2 <120447128+EvelinHollik2@users.noreply.github.com>
2023-11-29 08:33:13 +0100

    Update Program.cs

--- WindowsFormPizza/Program.cs
+++ WindowsFormPizza/Program.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,27 +20,3 @@ namespace WindowsFormPizza
         }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace WindowsFormPizza
-{
-    internal static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
-    }
-}
->>>>>>> 51da15f225cf15f9e335bd3f2ee5749c48e53b71
