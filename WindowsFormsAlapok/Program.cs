25d3cffe88f6d04b4f9319c12c978c087874f4fd
EvelinHollik2 <120447128+EvelinHollik2@users.noreply.github.com>
2024-04-25 09:07:05 +0200

    Update Program.cs

--- WindowsFormsAlapok/Program.cs
+++ WindowsFormsAlapok/Program.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,27 +20,3 @@ namespace WindowsFormsAlapok
         }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace WindowsFormsAlapok
-{
-    internal static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
-    }
-}
->>>>>>> 51da15f225cf15f9e335bd3f2ee5749c48e53b71
